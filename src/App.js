07a9521c47415a4d0f372bcac81daa698668f140
sodamkimkim <theka265@gmail.com>
2024-04-13 21:38:28 +0900

    Updated App.js - style적용하는 여러가지 방법

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
 import "./App.css";
+
 export default class App extends Component {
+  btnStyle = {
+    color: "#fff",
+    border: "none",
+    padding: "5px 9px",
+    borderRadius: "50%",
+    cursor: "pointer",
+    float: "right"
+  }
+  getStyle = () => {
+    return {
+      padding: "20px",
+      borderBottom: "1px dotted #ccc",
+      TextDecoder: 'none'
+    }
+  }
+
   render() {
     return (
       <div className="container">
@@ -8,6 +25,17 @@ export default class App extends Component {
           <div className="title">
             <h1>할 일 목록</h1>
           </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} /> 공부하기
+            <button style={this.btnStyle}>x</button>
+          </div>
+
+          <div style={this.getStyle()}>
+            <input type="checkbox" defaultChecked={false} /> 청소하기
+            <button style={this.btnStyle}>x</button>
+          </div>
+
         </div>
       </div>
     )
