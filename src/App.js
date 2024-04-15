83c566d070c395ad92db2fa1d97b6148242ac194
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2024-04-15 13:43:54 +0900

    filter()이용해서 새로운 객체 배열 생성하기

--- src/App.js
+++ src/App.js
@@ -18,19 +18,23 @@ export default class App extends Component {
     }
   }
 
-  todoData = [
-    {
-      id: "1",
-      title: "공부하기",
-      complete: true
-    },
-    {
-      id: "2",
-      title: "방청소하기",
-      complete: false
-    }
-  ]
+todoData = [
+  {
+    id: "1",
+    title: "공부하기",
+    complete: true
+  },
+  {
+    id: "2",
+    title: "방청소하기",
+    complete: false
+  }
+]
 
+handleClick = (id)=>{ 
+  let newTodoData = this.todoData.filter((data)=>data.id!==id);
+  console.log('newTodoData', newTodoData);
+}
   render() {
     return (
       <div className="container">
@@ -39,14 +43,15 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
 
-          {this.todoData.map((data) => (
+          {this.todoData.map((data)=>(
             <div style={this.getStyle()} key={data.id}>
-              <input type="checkbox" defaultChecked={data.complete} /> {data.title}
-              <button style={this.btnStyle}>x</button>
+              <input type="checkbox" defaultChecked={data.complete}/>{data.title}
+              <button style={this.btnStyle} onClick={()=>this.handleClick(data.id)}>x</button>
             </div>
           ))}
         </div>
       </div>
     )
   }
-}
+
+} // end of class 
