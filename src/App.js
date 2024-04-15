d59853bc5b024654248822dee4550174800bb92d
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2024-04-15 13:54:57 +0900

    React State

--- src/App.js
+++ src/App.js
@@ -1,57 +1,62 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
 
 export default class App extends Component {
-  btnStyle = {
-    color: "#fff",
-    border: "none",
-    padding: "5px 9px",
-    borderRadius: "50%",
-    cursor: "pointer",
-    float: "right"
+
+  state={
+    todoData : [
+        {
+            id: "1",
+            title: "공부하기",
+            complete: true
+        }, {
+            id: "2",
+            title: "방청소하기",
+            complete: false
+        }
+    ], value:"",
   }
-  getStyle = () => {
-    return {
-      padding: "20px",
-      borderBottom: "1px dotted #ccc",
-      TextDecoder: 'none'
+    btnStyle = {
+        color: "#fff",
+        border: "none",
+        padding: "5px 9px",
+        borderRadius: "50%",
+        cursor: "pointer",
+        float: "right"
     }
-  }
+    getStyle = () => {
+        return {padding: "20px", borderBottom: "1px dotted #ccc", TextDecoder: 'none'}
+    }
 
-todoData = [
-  {
-    id: "1",
-    title: "공부하기",
-    complete: true
-  },
-  {
-    id: "2",
-    title: "방청소하기",
-    complete: false
-  }
-]
 
-handleClick = (id)=>{ 
-  let newTodoData = this.todoData.filter((data)=>data.id!==id);
-  console.log('newTodoData', newTodoData);
-}
-  render() {
-    return (
-      <div className="container">
-        <div className="todoBlock">
-          <div className="title">
-            <h1>할 일 목록</h1>
-          </div>
+    handleClick = (id) => {
+        let newTodoData = this.state
+            .todoData
+            .filter((data) => data.id !== id);
+            this.setState({todoData:newTodoData});
+        console.log('newTodoData', newTodoData);
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="todoBlock">
+                    <div className="title">
+                        <h1>할 일 목록</h1>
+                    </div>
 
-          {this.todoData.map((data)=>(
-            <div style={this.getStyle()} key={data.id}>
-              <input type="checkbox" defaultChecked={data.complete}/>{data.title}
-              <button style={this.btnStyle} onClick={()=>this.handleClick(data.id)}>x</button>
+                    {
+                        this.state
+                            .todoData
+                            .map((data) => (
+                                <div style={this.getStyle()} key={data.id}>
+                                    <input type="checkbox" defaultChecked={data.complete}/>{data.title}
+                                    <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
+                                </div>
+                            ))
+                    }
+                </div>
             </div>
-          ))}
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 
-} // end of class 
+} // end of class
