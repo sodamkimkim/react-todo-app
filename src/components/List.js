e377605f12274b014748d3f5f6d764931791e050
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2024-04-17 17:09:56 +0900

    TailCss적용과 DragDrop 구현

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,5 @@
-import React from "react"; 
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import React from "react";
+import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
 
 export default function List({todoData, setTodoData}) {
 
@@ -18,30 +18,42 @@ export default function List({todoData, setTodoData}) {
         setTodoData(newTodoData);
         console.log('newTodoData', newTodoData);
     };
-    const handleEnd=(result)=>{
+    const handleEnd = (result) => {
         console.log(result);
+        if(!result.destination) return;
+        const newTodoData = todoData;
+        // 1. 변경시키는 아이템을 배열에서 지워준다.
+        // 2. return 값으로 지워진 아이템을 잡아준다.
+        const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+
+        // 원하는 자리에 reorderItem을 insert
+        newTodoData.splice(result.destination.index, 0, reorderedItem);
+        setTodoData(newTodoData);
+        console.log(todoData);
     }
     return (
         <div>
             <DragDropContext onDragEnd={handleEnd}>
-                <Droppable  droppableId="todoListDroppable">
+                <Droppable id="todoList" droppableId="todoList">
                     {
                         (provided) => (
-                            <div {...provided.droppableProps} ref={provided.innerRef}>
+                            <div   {...provided.droppableProps} ref={provided.innerRef}>
                                 {
                                     todoData.map((data, index) => (
-                                        <Draggable key={data.id} draggableId={data.id.toString()}
+                                        <Draggable
+                                            key={data.id}
+                                            draggableId={data
+                                                .id
+                                                .toString()}
                                             index={index}>
                                             {
                                                 (provided, snapshot) => (
-                                                    <div
+                                                    <div key={data.id}
+                                                        {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}
                                                         className={`${snapshot.isDragging
                                                             ? "bg-gray-400"
                                                             : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600  border rounded`}
-                                                        key={data.id}
-                                                        {...provided.droppableProps}
-                                                        ref={provided.innerRef}
-                                                        {...provided.dragHandleProps}>
+                                                        >
                                                         <div className="items-center">
                                                             <input
                                                                 className="mr-5"
